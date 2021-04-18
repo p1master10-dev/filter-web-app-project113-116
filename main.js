4ed8ae1ad2c7dec1e9ff8924a5e13c87db4fe5c6
p1master10-dev <73630187+p1master10-dev@users.noreply.github.com>
2021-04-18 12:53:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,11 +5,30 @@ function preload() {
 function setup() {
      canvas = createCanvas(300, 300);
      canvas.center();
+     video = createCapture(VIDEO);
+     video.size(300, 300);
+     video.hide();
+
+     poseNet = ml5.poseNet(video, modelLoaded);
+     poseNet.on('pose', gotPoses);
 }
 
+function modelLoaded() {
+     console.log('PoseNet loaded !');
+}
+
+function gotPoses(results) {
+     if (results.length > 0) {
+          console.log(results);
+          console.log('eye1 x = ' + results[0].pose.leftEye.x);
+          console.log('eye1 y = ' + results[0].pose.leftEye.y);
+          console.log('eye2 x = ' + results[0].pose.rightEye.x);
+          console.log('eye2 y = ' + results[0].pose.rightEye.y);
+     }
+}
 
 function draw() {
-     
+     image(video, 0, 0, 300, 300);
 }
 
 function take_snapshot() {
